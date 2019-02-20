54ba58edcb06fa54033b7c109a49104a114e4255
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2019-02-20 10:35:32 -0700

    Fixed overflow issue, need to sort out ASCII art and formatting

--- intense/poc.c
+++ intense/poc.c
@@ -7,9 +7,14 @@ int main(void) {
 	char str[50] = "\000";
 	char new_str[50] = "\000";
 	int counter = 0;
+	int warn_chars = 0;
+	system("clear");
 	while(1) {
+		printf("%s\n\n\n\n", new_str);
+		if(warn_chars) {
+			printf("You're adding a few too many characters there, buddy...\n");
+		}
 		printf("%s", new_str);
-		printf("\n\n%s", new_str);
 
 		strcpy(str, new_str);
 
@@ -26,11 +31,13 @@ int main(void) {
 				new_str[counter-1] = '\000';
 				--counter;
 			}
-		} else {
+		} else if(counter < 50){
 			++counter;
 
 			strcpy(new_str, str);
 			strcat(new_str, c);
+		} else {
+			warn_chars = 1;
 		}
 
 	}
