ec8bc6993b341f14f34e3ea8f911be82d96e6409
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2019-02-20 10:40:09 -0700

    Deleted redundant files

--- helloworld_backup.s
+++
@@ -1,9 +0,0 @@
-.LC0:	.string	"Hello, world!"
-	.globl	main
-main:	pushq	%rbp
-	movq	%rsp, %rbp
-	leaq	.LC0(%rip), %rdi
-	call	puts@PLT
-	xor	%eax, %eax
-	popq	%rbp
-	ret
--- l.java
+++
@@ -1,5 +0,0 @@
-class l {
-    public static void main(String[] args) {
-        System.out.print("l"); // Display the string.
-    }
-}
